cf5e530a8cd778b95042d52e46481d621bf06958
SudeepaTechrida <sudeepa@techrida.com>
2023-08-17 15:35:26 +0545

    Edited task

--- csvTask.py
+++ csvTask.py
@@ -1,20 +1,70 @@
-class CSV_Saver:
-    def __init__(self):
-        self.name = "Parent CSV"
-    def csv_file(self):
-        self.csv_file = self.name + ".csv"
-    def createCSV(self):
-        self.file = open(self.csv_file, 'w',newline='')
-        self.file.write('Hello \nWelcome \nto Python')
-        self.file.close()
-class ChildCSV_Saver(CSV_Saver):
-    def __init__(self):
-        self.name = "Child CSV"
-    def csv_file(self):
-        self.csv_file = self.name + ".csv"
+class Music:
+    def __init__(self, name):
+        self.name = name 
 
-objParent = CSV_Saver()
-objChild = ChildCSV_Saver()
+    @staticmethod 
+    def createCSV(name):
+        file = open(name, 'a')
 
-objChild.csv_file()
-objChild.createCSV()
+    def addDetails(self, data):
+        file = open(self.name, 'a')
+        file.write(",".join(data)+"\n")
+        file.close()
+        print("Music added.")
+    def readDetails(self):
+        detail = []
+        file = open(self.name, 'r')
+        details = file.read()
+        for line in details:
+            split1 = line.strip().split(",")
+            detail.append(split1)
+        return detail
+    
+    def editDetails(self, dataId):
+        data =  self.readDetails()
+        value = data[dataId]
+        return value
+    def update(self, data,dataId):
+        editData = self.editDetails(dataId)
+        if 0 <= dataId < len(editData):
+            editData[dataId] = data
+            file = open(self.name, 'a+')
+            file.write(','.join(data)+'/n')
+            return True
+        return False
+    def delete(self, deleteID):
+        deleteData = self.editDetails(int(deleteID))
+        rows = self.readDetails()
+        if 0 <= int(deleteID) <len(deleteData):
+            deleteData[dataId]= ''
+            file = open(self.name, 'w')
+            for row in rows:
+                file.write(','.join(row)+'\n')
+            file.close()
+            return True
+        return False
+
+        
+class Korean(Music):
+     def __init__(self, filename):
+        super().__init__(filename)
+
+csvName = input("Enter the name to save the csv file with .csv extension: ")
+
+
+musicChild = Korean.createCSV(csvName)
+musicChild = Korean(csvName)
+
+musicId = int(input("Id: "))
+musicName = str(input("Name of the song: "))
+musicBand = str(input("Name of the artist: "))
+
+if musicId and musicBand and musicName is not None:
+    musicChild.addDetails([str(musicId), musicName, musicBand])
+    print(musicChild.readDetails())
+    dataId = int(input("Enter the id you want to edit: "))
+    musicChild.editDetails(dataId)
+    data = input("Enter the changes you want: ")
+    musicChild.update(data, dataId)
+    deleteId = input("Enter the data number you want to delete: ")
+    musicChild.delete(deleteId)
--- icecreamTask.py
+++ icecreamTask.py
@@ -31,16 +31,17 @@ def main():
     print("------CONE TYPE------")
     for cone,conePrice in cone_menu.items():
         print(cone," : ", conePrice)
+ 
+    icecream = str(input("Enter the icecream of your choose: "))
+
+    cones = str(input("Enter the cone you want: "))
     try:
-        icecream = str(input("Enter the icecream of your choose: "))
-
-        cone = str(input("Enter the cone you want: "))
-
         scoops = int(input("How many scoops do you want? "))
-    
-    
-        if icecream in icecream_menu or cone in cone_menu or scoops is not None:
-            obj_icecream = Icecream(icecream, icecream_menu[icecream], cone ,cone_menu[cone], scoops)
+    except ValueError:
+        scoops = int(input("How many scoops do you want? "))
+    while True:
+        if icecream in icecream_menu and cones in cone_menu and scoops is not None:
+            obj_icecream = Icecream(icecream, icecream_menu[icecream], cones ,cone_menu[cones], scoops)
             obj_icecream.icecream_Name()
             obj_icecream.calculate()
             csvFile = input("Do you want to download the menu? \nYes or No ")
@@ -52,29 +53,20 @@ def main():
                     file.write(" ")
                     file.write(str(price))
                     file.write("\n")
-                file.close()
-                
+                file.close() 
             else:
                 print("Thank you!!")
+            break
         else:
+            print("in else")
             print("Please enter the flavor of icecream you want and your choice of cone again")
-
-            print("-----ICECREAM MENU------")
-            for ice,price in icecream_menu.items():
-                print(ice , ": ", price)
-
-            print("------CONE TYPE------")
-            for cone,conePrice in cone_menu.items():
-                print(cone," : ", conePrice)
-            
+                    
             icecream = str(input("Enter the icecream of your choose: "))
 
-            cone = str(input("Enter the cone you want: "))
+            cones = str(input("Enter the cone you want: "))
 
             scoops = int(input("How many scoops do you want? "))
-
-    except:
-        print("Error")
+          
 
 if __name__=="__main__":
     main()
