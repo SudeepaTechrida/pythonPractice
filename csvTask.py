4c86f4df1bb8503139e942f5063852112f49b054
SudeepaTechrida <sudeepa@techrida.com>
2023-08-21 12:41:23 +0545

    task edited

--- csvTask.py
+++ csvTask.py
@@ -1,66 +1,61 @@
-class Music:
+class CSVSaver:
     def __init__(self, name):
-        self.name = name 
-
-    @staticmethod 
-    def createCSV(name):
-        file = open(name, 'a')
- 
-    def addDetails(self, data):
-        file = open(self.name, 'a')
+        self.name = name
+    
+    def addDetails(self, data,):
+        file = open(self.name + '.csv', 'a')
         file.write(",".join(data)+"\n")
         file.close()
-        print("Music added.")
+        print("Object added.")
     def readDetails(self):
-        file = open(self.name, 'r')
+        file = open(self.name + '.csv', 'r')
         details = file.readlines()
         return details 
 
     def update(self, data,dataId):
         editData = self.readDetails()
-        # print(editData)
-        if 0 <= dataId < len(editData):
-            editData[dataId] = data
-            print(editData[dataId])
-            file = open(self.name, 'a+')
-            file.write(data)
-            file.write("\n")
-            # return True
-        return False   
+        oneData = editData[dataId]
+        if dataId >=0:
+            file = open(self.name+'.csv', 'w')
+            if oneData in editData:
+                file.writelines(','.join(data)+'\n')
+            else:
+                file.write(','.join(editData)+'\n')
+            file.close()
+            print("Object Edited.")
+
 
     def delete(self, deleteID):
         rows = self.readDetails()
-        print(rows)
-        if 0 <= int(deleteID):
-            if rows[int(deleteID)] in rows:
-                rows[int(deleteID)] == ''
-                file = open(self.name,'w+')
-                file.write(rows[int(deleteID)])
-                # for i in rows:
-                #     file.write(i)
-                file.close()
-        return False
-
+        deletedata = rows[deleteID]
+        file = open(self.name+'.csv', 'w')
+        if deletedata:
+            deletedata = ''
+            file.write(deletedata)
+        else:
+            file.write(','.join(rows)+'\n')
+        file.close()
+        print("Object Deleted")
         
-class Korean(Music):
-     def __init__(self, filename):
-        super().__init__(filename)
+class Music(CSVSaver):
+    def __init__(self, id, name, artist):
+        self.name = name
+        self.id = id
+        self.artist = artist
+class Car(CSVSaver):
+    def __init__(self,id,name,model):
+        self.name = name
+        self.id = id
+        self.model = model
+    
+# music_inst = Music('1', 'SARA SARA', 'Seventeen')
+# music_inst.addDetails(['1','SEVENTEEN', 'SARA SARA'])
+# print(music_inst.readDetails())
+# music_inst.update(['1','Sara Sara', 'SEVENTEEN'],0)
+# music_inst.delete(0)
 
-csvName = input("Enter the name to save the csv file with .csv extension: ")
-
-
-musicChild = Korean.createCSV(csvName)
-musicChild = Korean(csvName)
-
-musicId = int(input("Id: "))
-musicName = str(input("Name of the song: "))
-musicBand = str(input("Name of the artist: "))
-
-if musicId and musicBand and musicName is not None:
-    musicChild.addDetails([str(musicId), musicName, musicBand])
-    print(musicChild.readDetails())
-    dataId = int(input("Enter the id you want to edit: "))
-    data = input("Enter the changes you want: ")
-    musicChild.update(data, dataId)
-    deleteId = input("Enter the data number you want to delete: ")
-    musicChild.delete(deleteId)
+car_obj = Car(1,'Boleno','Suziki')
+car_obj.addDetails(['1','Boleno','Suziki'])
+car_obj.readDetails()
+car_obj.update(['1','A-Star','Suzuki'],0)
+car_obj.delete(0)
