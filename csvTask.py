a1979d523db523aa744e6c2df5a9de61c16deab1
SudeepaTechrida <sudeepa@techrida.com>
2023-08-22 11:05:38 +0545

    Update and delete function working

--- csvTask.py
+++ csvTask.py
@@ -1,61 +1,54 @@
 class CSVSaver:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = "CSV.csv"
     
-    def addDetails(self, data,):
-        file = open(self.name + '.csv', 'a')
+    def addDetails(self, data):
+        file = open(self.name, 'a')
         file.write(",".join(data)+"\n")
         file.close()
         print("Object added.")
     def readDetails(self):
-        file = open(self.name + '.csv', 'r')
+        file = open(self.name, 'r')
         details = file.readlines()
         return details 
-
-    def update(self, data,dataId):
-        editData = self.readDetails()
-        oneData = editData[dataId]
-        if dataId >=0:
-            file = open(self.name+'.csv', 'w')
-            if oneData in editData:
-                file.writelines(','.join(data)+'\n')
-            else:
-                file.write(','.join(editData)+'\n')
-            file.close()
-            print("Object Edited.")
-
-
-    def delete(self, deleteID):
-        rows = self.readDetails()
-        deletedata = rows[deleteID]
-        file = open(self.name+'.csv', 'w')
-        if deletedata:
-            deletedata = ''
-            file.write(deletedata)
-        else:
-            file.write(','.join(rows)+'\n')
-        file.close()
-        print("Object Deleted")
-        
-class Music(CSVSaver):
-    def __init__(self, id, name, artist):
-        self.name = name
-        self.id = id
-        self.artist = artist
-class Car(CSVSaver):
-    def __init__(self,id,name,model):
-        self.name = name
-        self.id = id
-        self.model = model
     
-# music_inst = Music('1', 'SARA SARA', 'Seventeen')
-# music_inst.addDetails(['1','SEVENTEEN', 'SARA SARA'])
-# print(music_inst.readDetails())
-# music_inst.update(['1','Sara Sara', 'SEVENTEEN'],0)
-# music_inst.delete(0)
+    def update(self, dataId, data):
+       readdata = self.readDetails()
+       if 0 <= dataId < len(readdata):
+           readdata[dataId] = ','.join(data) + '\n'
+           updateFile = open(self.name, 'w')
+           updateFile.writelines(readdata)
+           print("Row updated")
+       else:
+           print("Row index out of range.")
+           
+    @staticmethod
+    def delete(name,deleteID):
+        readData = open(name,'r')
+        detail = readData.readlines()
+        if 0 <= deleteID < len(detail):
+            detail[deleteID] = ''
+            deleteFile = open(name, 'w')
+            deleteFile.writelines(detail)
+            print("Row Deleted")
+        else:
+            print("Row index out of bound")
+class Music(CSVSaver):
+    def __init__(self, name):
+        self.name = name
+class Car(CSVSaver):
+    def __init__(self, name):
+        self.name = name
+    
+music_inst = Music("Music.csv")
+music_inst.addDetails(['1','SEVENTEEN', 'SARA SARA'])
+print(music_inst.readDetails())
+music_inst.update(0,['1','Sara Sara', 'SEVENTEEN'])
+Music.delete("Music.csv",0)
 
-car_obj = Car(1,'Boleno','Suziki')
+car_obj = Car("Car.csv")
+# print(car_obj.name)
 car_obj.addDetails(['1','Boleno','Suziki'])
-car_obj.readDetails()
-car_obj.update(['1','A-Star','Suzuki'],0)
-car_obj.delete(0)
+print(car_obj.readDetails())
+car_obj.update(0,['1','A-Star','Suzuki'])
+Car.delete("Car.csv", 0)
